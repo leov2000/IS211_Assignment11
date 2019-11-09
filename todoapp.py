5f651d208e7771561e707b080d0f0916719c4e73
leo <leovergarah@gmail.com>
2019-11-08 23:13:35 -0500

    (Added) form validations & error template

--- todoapp.py
+++ todoapp.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-import random
+from flask import Flask, render_template, request, redirect, url_for, render_template_string
+import random, re
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,6 +12,46 @@ def index():
     ]
     
     return render_template('index.html', todo_list=todo_list, cache_bust=random.random())
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    form_values = request.form
+    dict_form_values = dict(form_values)
+    validations = run_validations(dict_form_values)
+    
+    if('Incorrect' in validations.values()):
+        return render_template_string(invalid_form_values(), form=validations)
+
+    return redirect(url_for('index'))
+
+def invalid_form_values():
+    return"""
+    <h2>Please correct the following incorrect fields:</h2>
+        <div>
+            <span>Task:</span>
+            <span>{{form.task}}</span>
+        </div>
+        <div>
+            <span>Email:</span>
+            <span>{{form.email}}</span>
+        </div>
+        <div>
+            <span>Priority</span>
+            <span>{{form.priority}}</span>
+        </div>
+    """
+
+def validations_config():
+    return {
+        'task': lambda task: 'Correct' if task else 'Incorrect',
+        'email': lambda email: 'Correct' if re.search('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email) else 'Incorrect',
+        'priority':  lambda p: 'Correct' if p.lower() == 'low' or p.lower() == 'hard' or p.lower() == 'medium' else 'Incorrect'
+    }    
+
+def run_validations(form_dict):
+    validations = validations_config()
+
+    return dict([(k, validations[k](v)) for k, v in form_dict.items()])  
 
 if __name__ == '__main__':
     app.run()
