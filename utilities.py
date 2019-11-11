9d40c2fbcba82f1fbd2d57cf26f15efe6c79cfef
leo <leovergarah@gmail.com>
2019-11-10 21:29:29 -0500

    (Added) doc strings for utility methods

--- utilities.py
+++ utilities.py
@@ -1,6 +1,13 @@
 import json, re
 
 def read_todo_file():
+    """
+    A utility function that reads the json file.
+
+    Parameters: None.
+
+    Returns:  A list of todos or an empty list. 
+    """
     try:
         todo_list = json.loads(open('todo-list.json').read())
 
@@ -10,6 +17,13 @@ def read_todo_file():
     return todo_list
 
 def delete_todo_from_file(todo):
+    """
+    A utility function that reads/deletes/writes to a json file.
+    
+    Parameters: UUID identifying the todo.
+
+    Returns: None
+    """
     try:
         json_list = json.loads(open('todo-list.json').read())
 
@@ -23,6 +37,13 @@ def delete_todo_from_file(todo):
             json.dump(json_list, file)
 
 def validations_config():
+    """
+    A validation utility function that checks the inputs when they're submitted.
+
+    Parameters: None
+
+    Returns: A validation dictionary labeled by input.
+    """
     return {
         'task': lambda task: 'Correct' if task else 'Incorrect',
         'email': lambda email: 'Correct' if re.search('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email) else 'Incorrect',
@@ -30,11 +51,23 @@ def validations_config():
     }    
 
 def run_validations(form_dict):
+    """
+    A validation utility function that runs the validations for the inputs.
+
+    Parameters: form values in a dictionary format
+
+    Returns: A validation dictionary that's read by the submit route. If a incorrect value is present, the error template will be rendered.
+    """
     validations = validations_config()
 
     return dict([(k, validations[k](v)) for k, v in form_dict.items()])
 
 def invalid_form_template():
+    """
+    The invalid form template used when a value fails validation. 
+    
+    Returns: A HTML string.
+    """
     return"""
     <h2>Please correct the following incorrect fields:</h2>
         <div>
